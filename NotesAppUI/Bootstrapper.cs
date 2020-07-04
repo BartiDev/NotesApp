df864504716cf7255888338fb13df8022e28a2d4
Bartlomiej Nowak <bartlomiej.nowak22@gmail.com>
2020-07-04 20:44:11 +0200

    Add New Solution, WPF Project and Caliburn.micro.
    Adds solution to the app and WPF projecto for user interface with caliburn.micor set up.

---
+++ NotesAppUI/Bootstrapper.cs
@@ -0,0 +1,26 @@
+﻿using Caliburn.Micro;
+using NotesAppUI.ViewModels;
+using NotesAppUI.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace NotesAppUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+    }
+}
